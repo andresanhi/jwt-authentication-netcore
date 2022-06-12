97765a0074645e3607310a27ba7843b169b28a16
Andres Angulo <andresanhi@outlook.com>
2022-06-11 22:33:39 -0500

    +semver:fix First commit - Adding project layers. - Adding DI implementation

---
+++ src/Jwt.Authentication.Manager/Jwt.Authentication.Manager.Api/Config/Injections.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
+
+namespace Jwt.Authentication.Manager.Api.Config
+{
+    public static class Injections
+    {
+        public static void RegisterServices(this IServiceCollection services, IConfiguration configuration)
+        {
+        }
+    }
+}
